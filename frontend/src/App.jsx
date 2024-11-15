52c002664d4451c06b6ceee17542b2aeb2fe6569
Harsh Gangwal <harshgangwal24@gmail.com>
2024-11-15 11:55:22 +0530

    specialitymenu complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar'
 
 
 const App = () => {
-  return <div className='mx-4 sm:mx-[10%]' >
+  return <div className='mx-4 sm:mx-[15%]' >
    <Navbar />
     <Routes>
       <Route path ='/' element={<Home />} />
