8784a660374054251acb849f6464aa0e14a1553f
Harsh Gangwal <harshgangwal24@gmail.com>
2024-11-16 18:28:18 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+ const [state,setState] =useState('Sign Up')
 
+
+ const [email,setEmail] =useState('')
+ const [password,setPassword] =useState('')
+ const [name,setName] =useState('')
+
+ const onSubmitHandler =async (event) =>{
+  event.preventDefault()
+ }
 
   return (
-    <div>
-      
-    </div>
+    <form className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-9 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p>{state ==='Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state ==='Sign Up' ? "sign up" : "Log in"} to book appointment</p>
+        <div>
+          <p>FUll Name</p>
+          <input type="text" onChange={(e)=>setName(e.target.name)} value={name} required />
+        </div>
+        <div>
+          <p>Email</p>
+          <input type="email" onChange={(e)=>setName(e.target.name)} value={name} required/>
+        </div>
+        <div>
+          <p>Password</p>
+          <input type="password" onChange={(e)=>setName(e.target.name)} value={name} required />
+        </div>
+        <button>{state ==='Sign Up' ? "Create Account" : "Login"}</button>
+      </div>
+    </form>
   )
 }
 
